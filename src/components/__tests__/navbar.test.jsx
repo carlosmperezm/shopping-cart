5c27bba6f0caa2995e9b38355a05d71139a6a694
CarlosPerez <carlosmperez2901@gmail.com>
2025-09-08 14:44:16 -0700

    Add navigation bar tests

---
+++ src/components/__tests__/navbar.test.jsx
@@ -0,0 +1,42 @@
+import { describe, it, expect, beforeEach, afterEach } from "vitest";
+import {
+  render,
+  screen,
+  getAllByRole,
+  act,
+  cleanup,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { MemoryRouter } from "react-router";
+import NavBar from "../navbar";
+import * as matchers from "@testing-library/jest-dom/matchers";
+
+expect.extend(matchers);
+
+beforeEach(() => {
+  render(
+    <MemoryRouter>
+      <NavBar />
+    </MemoryRouter>
+  );
+});
+
+afterEach(cleanup);
+
+describe("NavBar component", () => {
+  it("renders correctly", () => {
+    const navbar = screen.getByRole("navigation");
+    expect(navbar).toBeInTheDocument();
+    const links = getAllByRole(navbar, "link");
+    links.forEach((link) => expect(link).toBeInTheDocument());
+  });
+  it("stays after links are clicked", () => {
+    const user = userEvent.setup();
+    const navbar = screen.getByRole("navigation");
+    const links = getAllByRole(navbar, "link");
+    links.forEach((link) => {
+      act(async () => await user.click(link));
+      expect(navbar).toBeInTheDocument();
+    });
+  });
+});
