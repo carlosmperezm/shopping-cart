83af0f22329bca9c971974ae50ad62fe74667bd9
CarlosPerez <carlosmperez2901@gmail.com>
2025-09-12 15:04:33 -0700

    Fix: test no  recognizing the navbar in the document

--- src/components/__tests__/navbar.test.jsx
+++ src/components/__tests__/navbar.test.jsx
@@ -1,30 +1,28 @@
-import { describe, it, expect, beforeEach, afterEach } from "vitest";
+import { describe, it, expect, afterEach } from "vitest";
 import {
   render,
   screen,
   getAllByRole,
-  act,
   cleanup,
+  waitFor,
 } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { MemoryRouter } from "react-router";
+import { MemoryRouter, Route, Routes } from "react-router";
 import NavBar from "../navbar";
 import * as matchers from "@testing-library/jest-dom/matchers";
+import { products } from "../../data/cart-items";
 
 expect.extend(matchers);
 
-beforeEach(() => {
-  render(
-    <MemoryRouter>
-      <NavBar />
-    </MemoryRouter>
-  );
-});
-
-afterEach(cleanup);
+afterEach(() => cleanup());
 
 describe("NavBar component", () => {
   it("renders correctly", () => {
+    render(
+      <MemoryRouter>
+        <NavBar productsInCart={products} />
+      </MemoryRouter>
+    );
     const navbar = screen.getByRole("navigation");
     expect(navbar).toBeInTheDocument();
     const links = getAllByRole(navbar, "link");
@@ -32,11 +30,27 @@ describe("NavBar component", () => {
   });
   it("stays after links are clicked", () => {
     const user = userEvent.setup();
+    render(
+      <MemoryRouter>
+        <NavBar productsInCart={products} />
+      </MemoryRouter>
+    );
     const navbar = screen.getByRole("navigation");
     const links = getAllByRole(navbar, "link");
     links.forEach(async (link) => {
-      await act(async () => await user.click(link));
+      await waitFor(async () => await user.click(link));
       expect(navbar).toBeInTheDocument();
     });
+  });
+  it("shows number of items in the cart", () => {
+    render(
+      <MemoryRouter>
+        <NavBar productsInCart={products} />
+      </MemoryRouter>
+    );
+    const cartLink = screen.getByRole("link", {
+      name: "Cart " + products.length,
+    });
+    expect(cartLink).toBeInTheDocument();
   });
 });
