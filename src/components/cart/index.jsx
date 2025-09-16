58d5b5167e1902b46aaaedd59955659ffec4f78d
CarlosPerez <carlosmperez2901@gmail.com>
2025-09-16 00:43:19 -0700

    Change the items' property so that it display the title of the products correctly

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -4,7 +4,7 @@ export default function Cart({ items }) {
   return (
     <ul>
       {items ? (
-        items.map((item) => <li key={item.id}>{item.name}</li>)
+        items.map((item) => <li key={item.id}>{item.title}</li>)
       ) : (
         <h2>Cart is empty</h2>
       )}
