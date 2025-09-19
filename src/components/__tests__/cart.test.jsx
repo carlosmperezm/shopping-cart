8094f3077048644544085f6566f47ac7c7aaf4dd
CarlosPerez <carlosmperez2901@gmail.com>
2025-09-18 21:49:43 -0700

    Add tests to the delete item function

--- src/components/__tests__/cart.test.jsx
+++ src/components/__tests__/cart.test.jsx
@@ -1,33 +1,87 @@
 import { afterEach, describe, expect, it } from "vitest";
-import { screen, render, cleanup } from "@testing-library/react";
+import { screen, render, cleanup, waitFor } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
-import Cart from "../cart";
+import { createMemoryRouter, Outlet, RouterProvider } from "react-router";
+import CartPage from "../../pages/cart";
+import ErrorPage from "../../pages/error";
+import { useState } from "react";
+import userEvent from "@testing-library/user-event";
 
 expect.extend(matchers);
 
+const mockProducts = [
+  { id: 1, title: "Apple", price: 0.99 },
+  { id: 2, title: "Banana", price: 0.59 },
+  { id: 3, title: "Orange", price: 0.79 },
+];
+
+function renderMockedApp(mockProducts) {
+  function MockApp() {
+    /*
+  Does not pull data from the api
+   */
+    // Mock products in the cart
+    const [productsInCart, setProductsInCart] = useState(mockProducts);
+    return (
+      <div id="content">
+        <Outlet context={[productsInCart, setProductsInCart]} />
+      </div>
+    );
+  }
+
+  const mockRoutes = [
+    {
+      path: "/",
+      Component: MockApp,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/cart",
+          Component: CartPage,
+        },
+      ],
+    },
+  ];
+
+  const router = createMemoryRouter(mockRoutes, { initialEntries: ["/cart"] });
+  render(<RouterProvider router={router} />);
+}
+
 afterEach(cleanup);
 
+//TODO: Finish all these tests
 describe("Cart Component", () => {
   it("renders", () => {
-    render(<Cart />);
+    renderMockedApp(mockProducts);
     const cart = screen.getByRole("list");
     expect(cart).toBeInTheDocument();
   });
   it("shows all items", () => {
-    const items = [
-      { id: 1, name: "Apple", price: 0.99 },
-      { id: 2, name: "Banana", price: 0.59 },
-      { id: 3, name: "Orange", price: 0.79 },
-    ];
-    render(<Cart items={items} />);
+    renderMockedApp(mockProducts);
     const elements = screen.getAllByRole("listitem");
-    expect(elements).toHaveLength(items.length);
+    expect(elements).toHaveLength(mockProducts.length);
   });
   it("shows a message when cart is empty", () => {
-    render(<Cart />);
+    renderMockedApp([]);
     const elements = screen.queryAllByRole("listitem");
     expect(elements).toHaveLength(0);
-    const message = screen.getByRole("heading");
+    const message = screen.getByText("Cart is empty");
     expect(message).toBeInTheDocument();
+  });
+  it("deletes elements", async () => {
+    const user = userEvent.setup();
+    renderMockedApp(mockProducts);
+    const listItems = screen.queryAllByRole("listitem");
+    const deleteButtons = screen.getAllByRole("button", { name: "Delete" });
+    await user.click(deleteButtons[0]);
+    expect(listItems[0]).not.toBeInTheDocument();
+    waitFor(() => {
+      expect(listItems).toHaveLength(mockProducts.length - 1);
+    });
+    await user.click(deleteButtons[0]);
+    expect(listItems[0]).not.toBeInTheDocument();
+    waitFor(() => {
+      expect(listItems).toHaveLength(mockProducts.length - 2);
+    });
   });
 });
--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -1,26 +1,23 @@
 import { useOutletContext } from "react-router";
 import "./styles.module.css";
 
-export default function Cart({ items }) {
-  // Todo: Create a button to eliminate the item of the cart.
+export default function Cart() {
   // Todo: Create a button to modify the quantity of the same products in the cart
 
   const [productsInCart, setProductsInCart] = useOutletContext();
 
   return (
     <ul>
-      {items.length > 0 ? (
-        items.map((item) => (
+      {productsInCart.length > 0 ? (
+        productsInCart.map((item) => (
           <li key={item.id} data-id={item.id}>
             {item.title} | Quantity: {item.quantity}
             <button
               onClick={(e) => {
                 const productId = e.target.parentNode.dataset.id;
-                console.log(typeof productId);
                 const newProducts = productsInCart.filter(
                   (product) => product.id !== Number(productId)
                 );
-                console.log(newProducts);
                 setProductsInCart(newProducts);
               }}
             >
@@ -29,7 +26,7 @@ export default function Cart({ items }) {
           </li>
         ))
       ) : (
-        <h2>Cart is empty</h2>
+        <p>Cart is empty</p>
       )}
     </ul>
   );
