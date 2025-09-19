e1ebbc871a24ada64108f6bfe9cd229d251619f9
CarlosPerez <carlosmperez2901@gmail.com>
2025-09-18 21:50:53 -0700

    Remove unecessary props in the Cart Page

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,12 +1,10 @@
 import Cart from "../../components/cart";
-import { useOutletContext } from "react-router";
 
 export default function CartPage() {
-  const cartItems = useOutletContext()[0];
   return (
     <main>
       <h1>Cart Page</h1>
-      <Cart items={cartItems} />
+      <Cart />
     </main>
   );
 }
