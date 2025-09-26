a89fb83fde6c637e22415f5aeecfed5419cfd4d3
CarlosPerez <carlosmperez2901@gmail.com>
2025-09-25 19:06:59 -0700

    Style cart items

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -1,35 +1,42 @@
 import { useOutletContext } from "react-router";
-import "./styles.module.css";
+import styles from "./styles.module.css";
 import QuantityInput from "../quantity-input";
+import { Trash2 } from "lucide-react";
 
 export default function Cart() {
   const [productsInCart, setProductsInCart] = useOutletContext();
 
   return (
-    <ul>
+    <ul className={styles.itemsContainer}>
       {productsInCart.length > 0 ? (
         productsInCart.map((item) => (
           <li key={item.id} data-id={item.id}>
-            {item.title}
-            {" | "}
-            <label>
-              Quantity
-              <QuantityInput
-                product={item}
-                setProduct={(updatedProduct) => {
-                  const updatedProducts = [];
-                  productsInCart.forEach((prod) => {
-                    if (prod.id === item.id) {
-                      updatedProducts.push(updatedProduct);
-                    } else {
-                      updatedProducts.push(prod);
-                    }
-                  });
-                  setProductsInCart(updatedProducts);
-                }}
-              />
-            </label>
+            <div className={styles.contentContainer}>
+              <div className={styles.imgContainer}>
+                <img src={item.image} />
+              </div>
+              <div className={styles.middleContainer}>
+                {item.title}
+                <label>
+                  <QuantityInput
+                    product={item}
+                    setProduct={(updatedProduct) => {
+                      const updatedProducts = [];
+                      productsInCart.forEach((prod) => {
+                        if (prod.id === item.id) {
+                          updatedProducts.push(updatedProduct);
+                        } else {
+                          updatedProducts.push(prod);
+                        }
+                      });
+                      setProductsInCart(updatedProducts);
+                    }}
+                  />
+                </label>
+              </div>
+            </div>
             <button
+              className={styles.deleteBtn}
               onClick={(e) => {
                 const productId = e.target.parentNode.dataset.id;
                 const newProducts = productsInCart.filter(
@@ -38,7 +45,7 @@ export default function Cart() {
                 setProductsInCart(newProducts);
               }}
             >
-              Delete
+              <Trash2 />
             </button>
           </li>
         ))
